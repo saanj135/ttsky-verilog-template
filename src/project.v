96d45497e5489d8b0ce203524cbe856fa546776c
Saanj Shetty <saanj135@gmail.com>
2026-03-03 19:50:02 -0800

    single vector accumulator

--- src/project.v
+++ src/project.v
@@ -16,12 +16,23 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+  reg [7:0] accum_reg;
+
+  always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+      accum_reg <= 8'd0;
+    end else if (ena) begin
+      accum_reg <= accum_reg + ui_in;
+    end
+  end
+
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  assign uo_out  = accum_reg;
+
+  assign uio_out = 8'd0;
+  assign uio_oe  = 8'hFF;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{uio_in, ena, 1'b0};
 
 endmodule
